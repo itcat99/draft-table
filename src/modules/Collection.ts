34b12fca3814c54b70a13980688d185a135d533e
busyRobots <sn11141117@qq.com>
2019-08-16 09:56:43 +0800

    修改：拆分Collection中的构思文档和代码逻辑 修改原因：明确职能划分

--- src/modules/Collection.ts
+++ src/modules/Collection.ts
@@ -1,22 +1,8 @@
 /**
- * Collection  一个集合类, 内部定义了相关的操作，主要是 遍历 查找 增加 删除 修改
+ * Collection  一个集合类, 内部定义了相关的操作，主要是 遍历 查找 增加 删除 修改;
+ * 主要的集合存储目标，数组 和 map
  */
 
-/**
- * 构思：
- * collection, 定义了一个集合，
- * 集合: （1）集合存储的内容 （2）对于集合的操作
- * 问题：
- *  1、 集合 存储的东西是 单一性？
- *  2、 集合操作的性能
- *  3、 外部使用的情况
- *  4、 如何应对 复合结构
- * -----
- *  1、确定 集合  array | object
- *  2、目标对象，{key: new map(),key: new map()}
- *
- *
- */
 import * as IS from "../helpers/is";
 class Collection {
   private items: Array<any> | Object;
